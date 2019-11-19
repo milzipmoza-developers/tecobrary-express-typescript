dc5899099705af969aa78ff18c31ce826606b033
mrkwon <erased11@gmail.com>
2019-11-20 01:05:08 +0900

    feat: depart test database environment

--- src/infra/database/sequelize.ts
+++ src/infra/database/sequelize.ts
@@ -1,13 +1,13 @@
 import {Sequelize} from "sequelize-typescript";
 
 export const sequelize = new Sequelize(
-    'tecoexpress',
+    process.env.MODE === 'test' ? 'test_tecoexpress' : 'tecoexpress',
     'luffy',
     '159456',
     {
         repositoryMode: true,
         dialect: 'mysql',
-        database: 'tecoexpress',
+        database: process.env.MODE === 'test' ? 'test_tecoexpress' : 'tecoexpress',
         username: 'root',
         password: '159456',
         storage: ':memory:',
