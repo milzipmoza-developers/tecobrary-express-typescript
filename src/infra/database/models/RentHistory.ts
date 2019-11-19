eef8d38e4a47cd41c8c68244d60a6974c5f029d6
mrkwon <erased11@gmail.com>
2019-11-19 19:13:49 +0900

    feat: model RentHistory

---
+++ src/infra/database/models/RentHistory.ts
@@ -0,0 +1,24 @@
+import {Column, Model, Table} from "sequelize-typescript";
+import {DataTypes} from "sequelize";
+
+@Table({
+    timestamps: true,
+    paranoid: true,
+    charset: 'utf8',
+    collate: 'utf8_general_ci',
+    tableName: 'RentHistories'
+})
+export class RentHistory extends Model<RentHistory> {
+
+    @Column({
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    })
+    serialId: number;
+
+    @Column({
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    })
+    userId: number;
+}
