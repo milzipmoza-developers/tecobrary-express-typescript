f1d517cd40a3f303d1cea5117c664ba33732d552
mrkwon <erased11@gmail.com>
2019-11-19 19:14:29 +0900

    feat: model WishBook

---
+++ src/infra/database/models/WishBook.ts
@@ -0,0 +1,51 @@
+import {Column, IsUrl, Model, Table} from "sequelize-typescript";
+import {DataTypes} from "sequelize";
+
+@Table({
+    timestamps: true,
+    paranoid: true,
+    charset: 'utf8',
+    collate: 'utf8_general_ci',
+    tableName: 'WishBooks'
+})
+export class WishBook extends Model<WishBook> {
+
+    @IsUrl
+    @Column({
+        type: DataTypes.STRING(255),
+        allowNull: true,
+    })
+    image: string;
+
+    @Column({
+        type: DataTypes.STRING(255),
+        allowNull: false,
+    })
+    title: string;
+
+    @Column({
+        type: DataTypes.STRING(255),
+        allowNull: false
+    })
+    author: string;
+
+    @Column({
+        type: DataTypes.STRING(255),
+        allowNull: true,
+    })
+    publisher: string;
+
+    @Column({
+        type: DataTypes.STRING(255),
+        allowNull: false,
+        unique: true,
+    })
+    isbn: string;
+
+    @Column({
+        type: DataTypes.STRING(255),
+        allowNull: true,
+        defaultValue: '내용 없음'
+    })
+    desc: string;
+}
