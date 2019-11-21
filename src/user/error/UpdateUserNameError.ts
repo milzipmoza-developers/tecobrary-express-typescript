02c421b2a0e635cc6169043146d9ccda2e3c6642
mrkwon <erased11@gmail.com>
2019-11-21 12:02:10 +0900

    feat: add UpdateUserError
    - UpdateUserError
    +- UpdateUserAuthorizationError
    +- UpdateUserNameError

---
+++ src/user/error/UpdateUserNameError.ts
@@ -0,0 +1,7 @@
+import {UpdateUserError} from "./UpdateUserError";
+
+export class UpdateUserNameError  extends UpdateUserError {
+    constructor() {
+        super("이름 업데이트 도중 오류가 발생하였습니다.");
+    }
+}
