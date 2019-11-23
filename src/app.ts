9b2b8a39191b2ea90cdf08bf201abd9e720a4678
mrkwon <erased11@gmail.com>
2019-11-23 11:21:32 +0900

    feat: add userRouter

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const morgan = require('morgan')
+const morgan = require('morgan');
+
+const userRouterFactory = require('./user/userRouterFactory');
 
 export const app = express();
 
@@ -9,6 +11,8 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(cors());
 
+app.use(userRouterFactory());
+
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.send('Welcome Tecobrary Api v1');
 });
