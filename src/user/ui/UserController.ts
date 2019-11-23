080be59e60e34635ed3eaf6cc8e535b465e575d8
mrkwon <erased11@gmail.com>
2019-11-23 18:12:13 +0900

    refactor: replace parseInt method to parseToNumber method

--- src/user/ui/UserController.ts
+++ src/user/ui/UserController.ts
@@ -1,4 +1,5 @@
 import * as UserService from '../service/UserService';
+import {parseToNumber} from "../../common/utils/stringParser";
 
 export const totalUsers = async (req, res) => {
     try {
@@ -15,8 +16,8 @@ export const totalUsers = async (req, res) => {
 
 export const pageUsers = async (req, res) => {
     try {
-        const page = parseInt(req.query.page);
-        const number = parseInt(req.query.number);
+        const page = parseToNumber(req.query.page);
+        const number = parseToNumber(req.query.number);
         const userList = await UserService.getPageableUsers(page, number);
         res.status(200).send(userList)
     } catch (error) {
@@ -28,7 +29,7 @@ export const pageUsers = async (req, res) => {
 
 export const findUserById =  async (req, res) => {
     try {
-        const id = parseInt(req.params.id);
+        const id = parseToNumber(req.params.id);
         const user = await UserService.findById(id);
         res.status(200).send(user)
     } catch (error) {
@@ -40,7 +41,7 @@ export const findUserById =  async (req, res) => {
 
 export const updateAuthorization = async (req, res) => {
     try {
-        const id = parseInt(req.body.id);
+        const id = parseToNumber(req.body.id);
         const newAuthorization = req.body.newAuthorization;
         await UserService.updateUserAuthorization(id, newAuthorization);
         const updatedUser = await UserService.findById(id);
@@ -54,7 +55,7 @@ export const updateAuthorization = async (req, res) => {
 
 export const updateName = async (req, res) => {
     try {
-        const id = parseInt(req.body.id);
+        const id = parseToNumber(req.body.id);
         const newName = req.body.newName;
         await UserService.updateUserName(id, newName);
         const updatedUser = await UserService.findById(id);
