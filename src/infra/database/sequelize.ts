120720a81f7a95bbcd8dcd9ca4d6e4680e113209
mrkwon <erased11@gmail.com>
2019-11-25 18:05:34 +0900

    refactor: add models list

--- src/infra/database/sequelize.ts
+++ src/infra/database/sequelize.ts
@@ -1,5 +1,13 @@
 import {Sequelize} from "sequelize-typescript";
 
+const models = [
+    __dirname + '/models/User.ts',
+    __dirname + '/models/LibraryBook.ts',
+    __dirname + '/models/WishBook.ts',
+    __dirname + '/models/RentHistory.ts',
+    __dirname + '/models/Serial.ts',
+];
+
 export const sequelize = new Sequelize(
     process.env.MODE === 'test' ? 'test_tecoexpress' : 'tecoexpress',
     'luffy',
@@ -11,5 +19,5 @@ export const sequelize = new Sequelize(
         username: 'root',
         password: '159456',
         storage: ':memory:',
-        models: [__dirname + '/models'],
-    })
+        models: models,
+    });
